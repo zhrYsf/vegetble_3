22a79a01dc2edcf7933a6f0f56fbdcbbff974952
yoseZzohar <klipim@gmail.com>
2017-02-09 14:40:32 +0200

    1

--- vegetable/src/pages/vl/vl.ts
+++ vegetable/src/pages/vl/vl.ts
@@ -2,17 +2,12 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import {DataV} from "../../service/DataV";
 import {Veg} from "../../model/veg";
-
-/*
-  Generated class for the VL page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-vl',
   templateUrl: 'vl.html'
 })
+
+
 export class VLPage {
   veglist:Veg[];
 
